f7ac74bec80b35f5eada9994e854c0cbfc844b78
Jason Smith <jasonrichardsmith@gmail.com>
2019-03-06 14:13:50 +0100

    edited fasiling test;

--- patch/patch_test.go
+++ patch/patch_test.go
@@ -1,12 +1,10 @@
 package patch
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetPatch(t *testing.T) {
-	p := GetPatch(0, 0, "mesh", "eu-west-1", "vn", "80")
-	fmt.Println("results")
-	fmt.Println(string(p))
+	GetPatch(0, 0, 0, "mesh", "eu-west-1", "vn", "DEBUG", "80", false)
+
 }

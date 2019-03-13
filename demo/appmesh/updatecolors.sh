2dd177755f72920efec44ec0c6fbda3ea1b63973
Qing Ju <juqing@amazon.com>
2019-03-12 18:07:24 -0700

    Added more webhook features

--- demo/appmesh/updatecolors.sh
+++ demo/appmesh/updatecolors.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-aws appmesh update-route --mesh-name ${MESH} --cli-input-json file://colors.r.json
+aws appmesh update-route --mesh-name ${MESH} --cli-input-json file://colors.r.2.json
